a6c7c94210dff9a3c8c9d0bb1ece32a089bdc981
Eckybrazzz <mike.e@vecks.com.br>
2022-02-14 18:12:03 -0300

    corrected path to image

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 // No need to import "react" just for JSX in React 17+
-import Recipes from "./Recipes";
-import sword from "../images/swc-sword.png";
-import swordSvg from "../images/sword.svg";
+// import Recipes from "./Recipes";
+// import sword from "../images/swc-sword.png";
+// import swordSvg from "../images/sword.svg";
+import paper from "../images/paper.png";
+import rock from "../images/rock.png";
+import scissors from "../images/scissors.png";
+
 
 import "../styles/index.scss";
 
@@ -10,12 +14,16 @@ const App = () => {
     <>
       <section className="hero"></section>
       <main>
-        <section>
-          <h1>Oh Herro, React.</h1>
-          <img src={sword} alt="sword" width="250" />
-          <img src={swordSvg} alt="sword" width="250" />
-          <Recipes />
-        </section>
+        <div class="container">
+          <h2>Challenge Rock, Paper, Scissors</h2>
+          <div class="flex-box-rps" id="flex-box-rps-div">
+            <img id="rock" src={rock} width="150" height="150" alt="" onclick="rpsGame(this)" />
+            <img id="paper" src={paper} width="150" height="150" alt="" onclick="rpsGame(this)" />
+            <img id="scissors" src={scissors} width="150" height="150" alt="" onclick="rpsGame(this)" />
+          </div>
+        
+          
+        </div>
       </main>
     </>
   );
